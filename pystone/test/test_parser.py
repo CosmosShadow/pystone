fa8c5de58ca0e571e37bd9c481da1e50b1eb374c
lichen <lichenarthurdata@gmail.com>
2017-09-29 09:57:50 +0800

    little change

--- pystone/test/test_parser.py
+++ pystone/test/test_parser.py
@@ -30,15 +30,7 @@ class TestParser(object):
 		(while (i < 10) ((if ((i % 2) == 0) (even = (even + i)) else (odd = (odd + i))) (i = (i + 1))))
 		(even + odd)"""
 
-		code_arr = codes.split('\n')
-		lexer = Lexer(code_arr)
-		basic_parser = BasicParser()
-
-		tree_arr = []
-		while lexer.peek(0) != Token.EOF:
-			tree = basic_parser.parse(lexer)
-			tree_arr.append(tree)
-
+		tree_arr = parse_code(codes)
 		for tree, target in zip(tree_arr, syntax_tree.split('\n')):
 			assert_equal(str(tree), target.strip())
 

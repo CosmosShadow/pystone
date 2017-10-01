8db15231f452a35974a5d8f4bd524d43d1caafc7
lichen <lichenarthurdata@gmail.com>
2017-10-01 17:33:27 +0800

    单元测试: Interpreter

--- pystone/test/test_interpreter.py
+++ pystone/test/test_interpreter.py
@@ -13,14 +13,18 @@ class TestInterpreter(object):
 	def setup(self):
 		pass
 
+	def test_interperter(self):
+		code = '''sum = 0
+		i = 1
+		while i < 10 {
+		    sum = sum + i
+		    i = i + 1
+		}
+		sum'''
+
+		results = default_run(code)
+		assert_equal(results, [0, 1, 10, 45])
+
 
 if __name__ == '__main__':
-	code = '''sum = 0
-	i = 1
-	while i < 10 {
-	    sum = sum + i
-	    i = i + 1
-	}
-	sum'''
-
-	default_run(code)
+	pass
